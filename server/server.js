ae67fc233ab6f96964c30cffeabb477495a76ae1
K.M. Mahbubuzzaman <iamdipto7@google.com>
2017-12-10 02:58:28 +0600

    greet new users and alert others

--- server/server.js
+++ server/server.js
@@ -15,7 +15,17 @@ app.use(express.static(publicPath));
 io.on('connection',(socket) => {
   console.log('New User connected');
 
+  socket.emit('newMessage',{
+    from: 'Admin',
+    text: 'Welcome to chat app',
+    createdAt: new Date().getTime()
+  });
 
+  socket.broadcast.emit('newMessage',{
+    from: 'Admin',
+    text: 'New user Joined',
+    createdAt: new Date().getTime()
+  });
 
   socket.on('createMessage',(message) => {
     console.log('Created at',message);
@@ -23,7 +33,12 @@ io.on('connection',(socket) => {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
-    })
+    });
+    // socket.broadcast.emit('newMessage',{
+    //       from: message.from,
+    //       text: message.text,
+    //       createdAt: new Date().getTime()
+    //     });
   });
 
   socket.on('disconnect',() => {
