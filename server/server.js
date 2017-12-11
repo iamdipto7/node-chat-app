24ed2b7738f2d99f3ef19926849ec323fc80e974
K.M. Mahbubuzzaman <iamdipto7@google.com>
2017-12-11 19:19:55 +0600

    scroll bottom

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ io.on('connection',(socket) => {
   socket.broadcast.emit('newMessage',generateMessage('Admin','New user joined'));
 
   socket.on('createMessage',(message,callback) => {
-    console.log('Created at',message);
+    //console.log('Created at',message);
     io.emit('newMessage',generateMessage(message.from,message.text));
     callback();
     // socket.broadcast.emit('newMessage',{
